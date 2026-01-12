1fe5151de13e50c306107bbd12efcf7aaca263db
Le Vivilet <levivilet@pm.me>
2026-01-12 15:56:11 +0100

    fix

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -9,7 +9,7 @@ const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 
 const sharedProcess = await import(sharedProcessUrl)
 
-process.env.PATH_PREFIX = '/status-bar-worker'
+process.env.PATH_PREFIX = '/main-area-worker'
 await sharedProcess.exportStatic({
   root,
   extensionPath: '',

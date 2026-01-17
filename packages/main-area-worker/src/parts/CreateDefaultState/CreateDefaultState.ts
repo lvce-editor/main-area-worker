19db1b78419fde4eb3d2abd1b335d497482fb89e
Le Vivilet <levivilet@pm.me>
2026-01-17 14:53:21 +0100

    fix

--- packages/main-area-worker/src/parts/CreateDefaultState/CreateDefaultState.ts
+++ packages/main-area-worker/src/parts/CreateDefaultState/CreateDefaultState.ts
@@ -3,9 +3,12 @@ import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
 export const createDefaultState = (): MainAreaState => {
   return {
     assetDir: '',
+    layout: {
+      activeGroupId: '0',
+      direction: 'horizontal',
+      groups: [],
+    },
     platform: 0,
-    statusBarItemsLeft: [],
-    statusBarItemsRight: [],
     uid: 0,
   }
 }

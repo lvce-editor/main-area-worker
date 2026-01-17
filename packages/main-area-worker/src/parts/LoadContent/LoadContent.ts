62e7aab4fabf6917966366f12f843b8741b72fab
Le Vivilet <levivilet@pm.me>
2026-01-17 15:04:24 +0100

    feature: in loadContent, load some tabs (#24)

--- packages/main-area-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/main-area-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,7 +1,37 @@
-import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
+import type { MainAreaState, Tab } from '../MainAreaState/MainAreaState.ts'
 
 export const loadContent = async (state: MainAreaState): Promise<MainAreaState> => {
+  const tabs: readonly Tab[] = [
+    {
+      content: '',
+      editorType: 'text',
+      id: '1',
+      isDirty: false,
+      title: 'tab 1',
+    },
+    {
+      content: '',
+      editorType: 'text',
+      id: '2',
+      isDirty: false,
+      title: 'tab 2',
+    },
+  ]
   return {
     ...state,
+    layout: {
+      activeGroupId: '0',
+      direction: 'horizontal',
+      groups: [
+        {
+          activeTabId: '',
+          direction: 'horizontal',
+          focused: false,
+          id: '0',
+          size: 300,
+          tabs,
+        },
+      ],
+    },
   }
 }

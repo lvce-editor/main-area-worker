cd374837864f37a4e47fb4e56099c6be086b124a
Le Vivilet <levivilet@pm.me>
2026-01-17 17:43:19 +0100

    feature: add handleClickCloseTab (#39)

---
+++ packages/main-area-worker/src/parts/HandleClickCloseTab/HandleClickCloseTab.ts
@@ -0,0 +1,6 @@
+import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
+
+export const handleClickCloseTab = (state: MainAreaState, groupId: string, tabId: string): MainAreaState => {
+  // TODO
+  return state
+}

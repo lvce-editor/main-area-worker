4edf5e261b7371bae3824f053e4e1a7050a02684
Le Vivilet <levivilet@pm.me>
2026-01-17 15:23:28 +0100

    feature: simplify virtual dom (#27)

--- packages/main-area-worker/src/parts/MainAreaMain/MainAreaMain.ts
+++ packages/main-area-worker/src/parts/MainAreaMain/MainAreaMain.ts
@@ -1,12 +1,12 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
-import * as MainAreaCommandMap from '../MainAreaCommandMap/MainAreaCommandMap.ts'
+import { commandMap } from '../CommandMap/CommandMap.ts'
 import { registerCommands } from '../MainAreaStates/MainAreaStates.ts'
 
 export const main = async (): Promise<void> => {
-  registerCommands(MainAreaCommandMap.mainAreaCommandMap)
+  registerCommands(commandMap)
   const rpc = await WebWorkerRpcClient.create({
-    commandMap: MainAreaCommandMap.mainAreaCommandMap,
+    commandMap: commandMap,
   })
   RendererWorker.set(rpc)
 }

c85429e73bf4192a009158c24648749772821c75
Le Vivilet <levivilet@pm.me>
2026-01-17 15:05:24 +0100

    feature: render basic dom

--- packages/main-area-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/main-area-worker/src/parts/RenderItems/RenderItems.ts
@@ -1,6 +1,8 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
+import { getMainAreaVirtualDom } from '../GetMainAreaVirtualDom/GetMainAreaVirtualDom.ts'
 
 export const renderItems = (oldState: MainAreaState, newState: MainAreaState): any => {
-  return [ViewletCommand.SetDom2, newState.uid, []]
+  const dom = getMainAreaVirtualDom(newState)
+  return [ViewletCommand.SetDom2, newState.uid, dom]
 }

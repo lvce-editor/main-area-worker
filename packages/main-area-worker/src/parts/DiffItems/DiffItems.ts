8b721876a96c9fde56f88fba2fa0488c8a5ea9b4
Le Vivilet <levivilet@pm.me>
2026-01-17 14:53:45 +0100

    fix

--- packages/main-area-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/main-area-worker/src/parts/DiffItems/DiffItems.ts
@@ -1,5 +1,5 @@
 import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
 
 export const isEqual = (oldState: MainAreaState, newState: MainAreaState): boolean => {
-  return oldState.statusBarItemsLeft === newState.statusBarItemsLeft && oldState.statusBarItemsRight === newState.statusBarItemsRight
+  return oldState.layout === newState.layout
 }

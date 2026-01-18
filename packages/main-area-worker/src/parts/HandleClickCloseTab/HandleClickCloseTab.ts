d57d713c3a1fb497849b1db81f04c408bfb96c09
Le Vivilet <levivilet@pm.me>
2026-01-18 13:36:28 +0100

    feature: when clicking on close tab, call closeTab (#63)

--- packages/main-area-worker/src/parts/HandleClickCloseTab/HandleClickCloseTab.ts
+++ packages/main-area-worker/src/parts/HandleClickCloseTab/HandleClickCloseTab.ts
@@ -1,6 +1,8 @@
 import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
+import { closeTab } from '../CloseTab/CloseTab.ts'
 
-export const handleClickCloseTab = (state: MainAreaState, groupId: number | string, tabId: number | string): MainAreaState => {
-  // TODO
-  return state
+export const handleClickCloseTab = (state: MainAreaState, rawGroupId: string, rawTabId: string): MainAreaState => {
+  const groupId = Number.parseInt(rawGroupId)
+  const tabId = Number.parseInt(rawTabId)
+  return closeTab(state, groupId, tabId)
 }

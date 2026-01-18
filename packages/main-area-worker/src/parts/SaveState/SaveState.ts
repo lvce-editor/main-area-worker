faf2f9a91091a0dbad74d71b01dc4414944cfa5b
Le Vivilet <levivilet@pm.me>
2026-01-18 13:38:07 +0100

    feature: improve saveState

--- packages/main-area-worker/src/parts/SaveState/SaveState.ts
+++ packages/main-area-worker/src/parts/SaveState/SaveState.ts
@@ -1,14 +1,9 @@
+import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
 import type { SavedState } from '../SavedState/SavedState.ts'
-import * as Assert from '../Assert/Assert.ts'
-import * as MainAreaStates from '../MainAreaStates/MainAreaStates.ts'
 
-export const saveState = (uid: number): SavedState => {
-  Assert.number(uid)
-  const value = MainAreaStates.get(uid)
-  const { newState } = value
-  const { statusBarItemsLeft, statusBarItemsRight } = newState
+export const saveState = (state: MainAreaState): SavedState => {
+  const { layout } = state
   return {
-    itemsLeft: statusBarItemsLeft,
-    itemsRight: statusBarItemsRight,
+    layout,
   }
 }

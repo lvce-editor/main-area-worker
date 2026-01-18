1ecb6e499e1811aa6773b6427dd80ffe0c95a341
Le Vivilet <levivilet@pm.me>
2026-01-18 15:04:36 +0100

    fix

--- packages/main-area-worker/src/parts/SaveMainAreaState/SaveMainAreaState.ts
+++
@@ -1,9 +0,0 @@
-import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
-
-export const saveMainAreaState = (state: MainAreaState): string => {
-  const { layout } = state
-  return JSON.stringify({
-    layout,
-    version: '1.0.0',
-  })
-}

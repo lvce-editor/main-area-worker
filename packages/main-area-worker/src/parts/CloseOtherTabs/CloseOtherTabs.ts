d3b72a548c93c188d2dbe531f0fef26cbfdf7093
Le Vivilet <levivilet@pm.me>
2026-01-18 14:55:08 +0100

    feature: add closeOtherTabs

---
+++ packages/main-area-worker/src/parts/CloseOtherTabs/CloseOtherTabs.ts
@@ -0,0 +1,36 @@
+import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
+
+export const closeOtherTabs = (state: MainAreaState, groupId: number): MainAreaState => {
+  const { layout } = state
+  const { groups } = layout
+
+  const group = groups.find((g) => g.id === groupId)
+  if (!group) {
+    return state
+  }
+
+  const { activeTabId } = group
+  if (activeTabId === undefined) {
+    return state
+  }
+
+  const newGroups = groups.map((g) => {
+    if (g.id === groupId) {
+      const newTabs = g.tabs.filter((tab) => tab.id === activeTabId)
+      return {
+        ...g,
+        activeTabId,
+        tabs: newTabs,
+      }
+    }
+    return g
+  })
+
+  return {
+    ...state,
+    layout: {
+      ...layout,
+      groups: newGroups,
+    },
+  }
+}

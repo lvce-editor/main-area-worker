2004d07787ad0ba6a32f629b0eacd02bf4eff53e
Le Vivilet <levivilet@pm.me>
2026-01-28 11:23:14 +0100

    fix

--- packages/main-area-worker/src/parts/OpenUri/OpenUri.ts
+++ packages/main-area-worker/src/parts/OpenUri/OpenUri.ts
@@ -76,6 +76,5 @@ export const openUri = async (state: MainAreaState, options: OpenUriOptions | st
   // After viewlet is created, mark it as ready
   // Attachment is handled automatically by virtual DOM reference nodes
   const { newState: readyState } = ViewletLifecycle.handleViewletReady(intermediateState1, tabWithViewlet.editorUid, tabWithViewlet.editorUid)
-  set(uid, state, readyState)
   return readyState
 }

b175971c428d421fbc8f513d739497b3994c850e
Le Vivilet <levivilet@pm.me>
2026-01-28 16:05:25 +0100

    feature: add create test (#117)

---
+++ packages/main-area-worker/test/CreateViewlet.test.ts
@@ -0,0 +1,99 @@
+import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import * as CreateViewlet from '../src/parts/CreateViewlet/CreateViewlet.ts'
+
+test('createViewlet should call Layout.createViewlet with correct parameters', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createViewlet': async () => {},
+  })
+
+  await CreateViewlet.createViewlet('test.viewlet', 1, 2, { height: 100, width: 100, x: 0, y: 0 }, 'file:///test.ts')
+
+  expect(mockRpc.invocations).toEqual([['Layout.createViewlet', 'test.viewlet', 1, 2, { height: 100, width: 100, x: 0, y: 0 }, 'file:///test.ts']])
+})
+
+test('createViewlet should handle different viewlet module ids', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createViewlet': async () => {},
+  })
+
+  await CreateViewlet.createViewlet('editor.text', 5, 10, { height: 200, width: 200, x: 10, y: 10 }, 'file:///path/to/file.ts')
+
+  expect(mockRpc.invocations).toEqual([
+    ['Layout.createViewlet', 'editor.text', 5, 10, { height: 200, width: 200, x: 10, y: 10 }, 'file:///path/to/file.ts'],
+  ])
+})
+
+test('createViewlet should handle different editorUids', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createViewlet': async () => {},
+  })
+
+  await CreateViewlet.createViewlet('test.viewlet', 42, 1, { height: 100, width: 100, x: 0, y: 0 }, 'file:///test.ts')
+
+  expect(mockRpc.invocations).toEqual([['Layout.createViewlet', 'test.viewlet', 42, 1, { height: 100, width: 100, x: 0, y: 0 }, 'file:///test.ts']])
+})
+
+test('createViewlet should handle different tabIds', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createViewlet': async () => {},
+  })
+
+  await CreateViewlet.createViewlet('test.viewlet', 1, 99, { height: 100, width: 100, x: 0, y: 0 }, 'file:///test.ts')
+
+  expect(mockRpc.invocations).toEqual([['Layout.createViewlet', 'test.viewlet', 1, 99, { height: 100, width: 100, x: 0, y: 0 }, 'file:///test.ts']])
+})
+
+test('createViewlet should handle different bounds', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createViewlet': async () => {},
+  })
+
+  const bounds = { height: 500, width: 500, x: 50, y: 50 }
+  await CreateViewlet.createViewlet('test.viewlet', 1, 2, bounds, 'file:///test.ts')
+
+  expect(mockRpc.invocations).toEqual([['Layout.createViewlet', 'test.viewlet', 1, 2, bounds, 'file:///test.ts']])
+})
+
+test('createViewlet should handle different uris', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createViewlet': async () => {},
+  })
+
+  await CreateViewlet.createViewlet('test.viewlet', 1, 2, { height: 100, width: 100, x: 0, y: 0 }, 'file:///path/to/another/file.js')
+
+  expect(mockRpc.invocations).toEqual([
+    ['Layout.createViewlet', 'test.viewlet', 1, 2, { height: 100, width: 100, x: 0, y: 0 }, 'file:///path/to/another/file.js'],
+  ])
+})
+
+test('createViewlet should be awaitable', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createViewlet': async () => {},
+  })
+
+  const result = await CreateViewlet.createViewlet('test.viewlet', 1, 2, { height: 100, width: 100, x: 0, y: 0 }, 'file:///test.ts')
+
+  expect(result).toBeUndefined()
+  expect(mockRpc.invocations).toEqual([['Layout.createViewlet', 'test.viewlet', 1, 2, { height: 100, width: 100, x: 0, y: 0 }, 'file:///test.ts']])
+})
+
+test('createViewlet should handle empty uri string', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createViewlet': async () => {},
+  })
+
+  await CreateViewlet.createViewlet('test.viewlet', 1, 2, { height: 100, width: 100, x: 0, y: 0 }, '')
+
+  expect(mockRpc.invocations).toEqual([['Layout.createViewlet', 'test.viewlet', 1, 2, { height: 100, width: 100, x: 0, y: 0 }, '']])
+})
+
+test('createViewlet should handle null bounds', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'Layout.createViewlet': async () => {},
+  })
+
+  await CreateViewlet.createViewlet('test.viewlet', 1, 2, null, 'file:///test.ts')
+
+  expect(mockRpc.invocations).toEqual([['Layout.createViewlet', 'test.viewlet', 1, 2, null, 'file:///test.ts']])
+})

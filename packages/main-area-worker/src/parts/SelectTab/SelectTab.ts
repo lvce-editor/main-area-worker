77742ca16b6ab6f0ea26a3fdb7f1c6adc040626f
Le Vivilet <levivilet@pm.me>
2026-01-28 16:34:23 +0100

    feature: load content when selecting tab (#119)

--- packages/main-area-worker/src/parts/SelectTab/SelectTab.ts
+++ packages/main-area-worker/src/parts/SelectTab/SelectTab.ts
@@ -1,6 +1,8 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { MainAreaState, Tab } from '../MainAreaState/MainAreaState.ts'
+import { createViewlet } from '../CreateViewlet/CreateViewlet.ts'
 import * as ExecuteViewletCommands from '../ExecuteViewletCommands/ExecuteViewletCommands.ts'
+import { findTabById } from '../FindTabById/FindTabById.ts'
 import * as GetNextRequestId from '../GetNextRequestId/GetNextRequestId.ts'
 import * as MainAreaStates from '../MainAreaStates/MainAreaStates.ts'
 import { startContentLoading } from '../StartContentLoading/StartContentLoading.ts'
@@ -123,6 +125,36 @@ export const selectTab = async (state: MainAreaState, groupIndex: number, index:
         }
         const createdState = ViewletLifecycle.createViewletForTab(newState, tabId, viewletModuleId, bounds)
         newState = createdState
+
+        // Store updated state before creating viewlet
+        MainAreaStates.set(uid, state, newState)
+
+        // Execute viewlet commands if any
+        if (switchCommands.length > 0) {
+          await ExecuteViewletCommands.executeViewletCommands(switchCommands)
+        }
+
+        // Get the tab to extract editorUid for viewlet creation
+        const tabWithViewlet = findTabById(newState, tabId)
+        if (tabWithViewlet) {
+          const { editorUid } = tabWithViewlet.tab
+          if (editorUid !== -1 && newTab.uri) {
+            // Create the actual viewlet instance
+            await createViewlet(viewletModuleId, editorUid, tabId, bounds, newTab.uri)
+
+            // Mark viewlet as ready
+            newState = ViewletLifecycle.handleViewletReady(newState, editorUid)
+            MainAreaStates.set(uid, state, newState)
+          }
+        }
+
+        // Start loading content in the background if needed
+        if (needsLoading && tab.uri) {
+          const latestState = await startContentLoading(state, newState, tabId, tab.uri, requestId)
+          return latestState
+        }
+
+        return newState
       }
     } catch {
       // Viewlet creation is optional - silently ignore if RendererWorker isn't available

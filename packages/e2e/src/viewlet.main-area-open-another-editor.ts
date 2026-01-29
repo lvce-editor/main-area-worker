4c77b5edf152517eea108fb792d074620de214ea
Le Vivilet <levivilet@pm.me>
2026-01-29 20:15:13 +0100

    feature: add open file e2e tests (#152)

---
+++ packages/e2e/src/viewlet.main-area-open-another-editor.ts
@@ -0,0 +1,20 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-open-another-editor'
+
+export const skip = 1
+
+export const test: Test = async ({ Editor, FileSystem, Main }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  const file1 = `${tmpDir}/file1.ts`
+  const file2 = `${tmpDir}/file2.ts`
+
+  await FileSystem.writeFile(file1, 'content1')
+  await FileSystem.writeFile(file2, 'content2')
+
+  // Open first editor
+  await Main.openUri(file1)
+  await Main.openUri(file2)
+
+  await Editor.shouldHaveText('content2')
+}
---
+++ packages/e2e/src/viewlet.main-area-open-file.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-open-file'
+
+export const skip = 1
+
+export const test: Test = async ({ Editor, FileSystem, Main }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  const testFile = `${tmpDir}/test.ts`
+  const testContent = 'export const hello = () => "world"'
+  await FileSystem.writeFile(testFile, testContent)
+
+  // act
+  await Main.openUri(testFile)
+
+  // assert
+  await Editor.shouldHaveText(testContent)
+}

d9af4c99e8c5499540f8abe108617adfdb7b3763
Le Vivilet <levivilet@pm.me>
2026-01-29 20:10:40 +0100

    fix

--- packages/e2e/src/viewlet.main-area-open-another-editor.ts
+++ packages/e2e/src/viewlet.main-area-open-another-editor.ts
@@ -1,7 +1,6 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-open-another-editor'
-export const skip = true
 
 const assert = (condition: boolean, message: string): void => {
   if (!condition) {
@@ -9,7 +8,7 @@ const assert = (condition: boolean, message: string): void => {
   }
 }
 
-export const test: Test = async ({ Command, FileSystem }) => {
+export const test: Test = async ({ Editor, FileSystem, Main }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const file1 = `${tmpDir}/file1.ts`
   const file2 = `${tmpDir}/file2.ts`
@@ -17,43 +16,9 @@ export const test: Test = async ({ Command, FileSystem }) => {
   await FileSystem.writeFile(file1, 'content1')
   await FileSystem.writeFile(file2, 'content2')
 
-  const uid = 5
-
-  await Command.execute('MainArea.create', uid, '', 0, 0, 800, 600, 0, tmpDir)
-
   // Open first editor
-  await Command.execute('MainArea.openUri', uid, {
-    focus: false,
-    preview: false,
-    uri: file1,
-  })
+  await Main.openUri(file1)
+  await Main.openUri(file2)
 
-  // Verify first editor is opened
-  let savedState = await Command.execute('MainArea.saveState', uid)
-  assert(savedState.layout.groups.length === 1, `Expected 1 group, got ${savedState.layout.groups.length}`)
-  assert(savedState.layout.groups[0].tabs.length === 1, `Expected 1 tab, got ${savedState.layout.groups[0].tabs.length}`)
-  assert(savedState.layout.groups[0].tabs[0].path === file1, `Expected tab path to be ${file1}`)
-  assert(savedState.layout.groups[0].activeTabId === savedState.layout.groups[0].tabs[0].id, 'First editor should be active')
-
-  // Open second editor
-  await Command.execute('MainArea.openUri', uid, {
-    focus: false,
-    preview: false,
-    uri: file2,
-  })
-
-  // Verify both tabs are visible
-  savedState = await Command.execute('MainArea.saveState', uid)
-  assert(savedState.layout.groups.length === 1, `Expected 1 group, got ${savedState.layout.groups.length}`)
-  assert(savedState.layout.groups[0].tabs.length === 2, `Expected 2 tabs, got ${savedState.layout.groups[0].tabs.length}`)
-
-  // Verify tab paths
-  const tab1 = savedState.layout.groups[0].tabs.find((t) => t.path === file1)
-  const tab2 = savedState.layout.groups[0].tabs.find((t) => t.path === file2)
-  assert(tab1 !== undefined, 'Tab with file1 should exist')
-  assert(tab2 !== undefined, 'Tab with file2 should exist')
-
-  // Verify only the second editor is active/visible
-  assert(savedState.layout.groups[0].activeTabId === tab2?.id, 'Second editor should be active')
-  assert(savedState.layout.groups[0].activeTabId !== tab1?.id, 'First editor should not be active')
+  await Editor.shouldHaveText('content2')
 }

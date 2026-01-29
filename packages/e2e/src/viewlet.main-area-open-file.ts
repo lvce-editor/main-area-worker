999199774fe93655817ec989d000895e9b3aee1f
Le Vivilet <levivilet@pm.me>
2026-01-29 20:09:40 +0100

    fix

--- packages/e2e/src/viewlet.main-area-open-file.ts
+++ packages/e2e/src/viewlet.main-area-open-file.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-open-file'
 
+export const skip = 1
+
 export const test: Test = async ({ Editor, FileSystem, Main }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

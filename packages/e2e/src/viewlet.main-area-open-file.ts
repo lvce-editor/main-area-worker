1b1b3d2ad546bcd0ec0691d6644fa8892b962f08
Le Vivilet <levivilet@pm.me>
2026-01-29 20:05:50 +0100

    fix

--- packages/e2e/src/viewlet.main-area-open-file.ts
+++ packages/e2e/src/viewlet.main-area-open-file.ts
@@ -1,6 +1,6 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const name = 'viewlet.main-area-open-uri'
+export const name = 'viewlet.main-area-open-file'
 
 export const test: Test = async ({ Editor, FileSystem, Main }) => {
   // arrange

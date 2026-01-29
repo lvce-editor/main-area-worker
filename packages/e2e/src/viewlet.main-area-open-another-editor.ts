1e9c3aeec2ffa1eca1ab2f22f9dd88dd5ea65e73
Le Vivilet <levivilet@pm.me>
2026-01-29 20:13:50 +0100

    skip

--- packages/e2e/src/viewlet.main-area-open-another-editor.ts
+++ packages/e2e/src/viewlet.main-area-open-another-editor.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-open-another-editor'
 
+export const skip = 1
+
 export const test: Test = async ({ Editor, FileSystem, Main }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const file1 = `${tmpDir}/file1.ts`

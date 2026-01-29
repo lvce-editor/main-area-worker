88e70f6e86ec25c05d5b0ce407891efdaffd42ed
Le Vivilet <levivilet@pm.me>
2026-01-29 23:14:26 +0100

    feature: remove unused properties from Tab interface

--- packages/main-area-worker/src/parts/Tab/Tab.ts
+++ packages/main-area-worker/src/parts/Tab/Tab.ts
@@ -2,20 +2,14 @@ import type { EditorType } from '../EditorType/EditorType.ts'
 import type { LoadingState } from '../LoadingState/LoadingState.ts'
 
 export interface Tab {
-  readonly content: string
-  readonly customEditorId?: string
   readonly editorType: EditorType
   readonly editorUid: number
   readonly errorMessage?: string
   readonly icon: string
   readonly id: number
-  readonly isAttached?: boolean
   readonly isDirty: boolean
   readonly language?: string
   readonly loadingState?: LoadingState
-  readonly loadRequestId?: number
   readonly title: string
   readonly uri?: string
-  readonly viewletInstanceId?: number
-  readonly viewletRequestId?: number
 }

1751186bd2a36057b0c95e06a7cb17bf9abbaf2d
Le Vivilet <levivilet@pm.me>
2026-01-29 20:05:34 +0100

    fix

--- packages/e2e/src/viewlet.main-area-open-uri.ts
+++ packages/e2e/src/viewlet.main-area-open-uri.ts
@@ -2,9 +2,9 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-open-uri'
 
-export const test: Test = async ({ Command, Editor, FileSystem, Main }) => {
+export const test: Test = async ({ Editor, FileSystem, Main }) => {
   // arrange
-  const tmpDir = await FileSystem.getTmpDir(Editor)
+  const tmpDir = await FileSystem.getTmpDir()
   const testFile = `${tmpDir}/test.ts`
   const testContent = 'export const hello = () => "world"'
   await FileSystem.writeFile(testFile, testContent)

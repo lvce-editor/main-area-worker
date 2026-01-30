be0adc0c4358282e0439fae57b0cffe14520232b
Le Vivilet <levivilet@pm.me>
2026-01-30 01:12:23 +0100

    fix

--- packages/e2e/src/viewlet.main-area-close-all-no-editors.ts
+++ packages/e2e/src/viewlet.main-area-close-all-no-editors.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-close-all-no-editors'
 
+export const skip = 1
+
 export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

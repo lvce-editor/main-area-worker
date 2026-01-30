fdc74f1cb4ae2d7d3b4c2052544fe260c57182b8
Le Vivilet <levivilet@pm.me>
2026-01-30 11:42:00 +0100

    feature: add tests for horizontal and vertical split functionality in main area (#192)

---
+++ packages/e2e/src/viewlet.main-area-open-editor-split-horizontal.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-split-horizontal'
+
+export const test: Test = async ({ Command, expect, FileSystem, Locator, Main }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  const file1 = `${tmpDir}/file1.ts`
+  const file2 = `${tmpDir}/file2.ts`
+  const file3 = `${tmpDir}/file3.ts`
+
+  await FileSystem.writeFile(file1, 'content1')
+  await FileSystem.writeFile(file2, 'content2')
+  await FileSystem.writeFile(file3, 'content3')
+
+  await Main.openUri(file1)
+
+  await Command.execute('Main.splitRight')
+
+  const editorGroups = Locator('.EditorGroup')
+  await expect(editorGroups).toHaveCount(2)
+}
---
+++ packages/e2e/src/viewlet.main-area-split-empty-group.ts
@@ -0,0 +1,16 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-split-empty-group'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, expect, Locator }) => {
+  // arrange
+
+  // act
+  await Command.execute('Main.splitRight')
+
+  // TODO: Add assertions for split functionality
+  const editorGroups = Locator('.EditorGroup')
+  await expect(editorGroups).toHaveCount(2)
+}

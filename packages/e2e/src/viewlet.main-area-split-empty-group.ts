00d7870aea8b4888fd06456c3963d23f2f9dd0de
Le Vivilet <levivilet@pm.me>
2026-01-30 11:39:33 +0100

    fix

--- packages/e2e/src/viewlet.main-area-split-empty-group.ts
+++ packages/e2e/src/viewlet.main-area-split-empty-group.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-split-empty-group'
 
-export const test: Test = async ({ FileSystem, Main }) => {
+export const test: Test = async ({ expect, Locator, Main }) => {
   // arrange
   await Main.closeAllEditors()
 
@@ -10,4 +10,6 @@ export const test: Test = async ({ FileSystem, Main }) => {
   await Main.splitRight()
 
   // TODO: Add assertions for split functionality
+  const editorGroups = Locator('.EditorGroup')
+  await expect(editorGroups).toHaveCount(2)
 }

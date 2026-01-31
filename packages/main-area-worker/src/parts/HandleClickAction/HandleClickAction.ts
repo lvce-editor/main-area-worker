362bdc369f14ddb4058913450357b9afcbc0193f
Le Vivilet <levivilet@pm.me>
2026-01-31 11:10:19 +0100

    feature: add close-group action to handleClickAction for closing editor groups (#199)

--- packages/main-area-worker/src/parts/HandleClickAction/HandleClickAction.ts
+++ packages/main-area-worker/src/parts/HandleClickAction/HandleClickAction.ts
@@ -1,9 +1,10 @@
 import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
+import { closeEditorGroup } from '../CloseEditorGroup/CloseEditorGroup.ts'
 import { GetActiveGroup } from '../GetActiveGroup/GetActiveGroup.ts'
 import * as GroupDirection from '../GroupDirection/GroupDirection.ts'
 import { splitEditorGroup } from '../SplitEditorGroup/SplitEditorGroup.ts'
 
-export const handleClickAction = async (state: MainAreaState, action: string): Promise<MainAreaState> => {
+export const handleClickAction = async (state: MainAreaState, action: string, rawGroupId?: string): Promise<MainAreaState> => {
   if (!action) {
     return state
   }
@@ -14,6 +15,14 @@ export const handleClickAction = async (state: MainAreaState, action: string): P
   }
 
   switch (action) {
+    case 'close-group':
+      if (rawGroupId) {
+        const groupId = Number.parseInt(rawGroupId, 10)
+        if (!Number.isNaN(groupId)) {
+          return closeEditorGroup(state, groupId)
+        }
+      }
+      return state
     case 'split-right':
       return splitEditorGroup(state, activeGroup.id, GroupDirection.Right)
     default:

bcf22c0ee280c0bc58bf22da96a2987b866eef13
Le Vivilet <levivilet@pm.me>
2026-02-02 17:48:18 +0100

    fix

--- packages/e2e/src/viewlet.main-area-save-no-editors.ts
+++ packages/e2e/src/viewlet.main-area-save-no-editors.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-save-no-editors'
 
-export const test: Test = async ({ Command, expect, FileSystem, Locator, Main, Workspace }) => {
+export const test: Test = async ({ Command, expect, FileSystem, Locator, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await Workspace.setPath(tmpDir)

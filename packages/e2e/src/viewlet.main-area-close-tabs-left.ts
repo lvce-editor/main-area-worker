d041e48590b1a2507a3c5b615bbf61397f93b742
Le Vivilet <levivilet@pm.me>
2026-02-02 15:04:55 +0100

    refactor: remove skip flag from multiple test files

--- packages/e2e/src/viewlet.main-area-close-tabs-left.ts
+++ packages/e2e/src/viewlet.main-area-close-tabs-left.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-close-tabs-left'
 
-// export const skip = 1
-
 export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

764b48336af5aea9d1e4331f0ca8886a938065b8
Le Vivilet <levivilet@pm.me>
2026-02-02 15:36:14 +0100

    fix

--- packages/e2e/src/viewlet.main-area-tab-switching.ts
+++ packages/e2e/src/viewlet.main-area-tab-switching.ts
@@ -2,24 +2,23 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-tab-switching'
 
-export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
+export const test: Test = async ({ Command, expect, FileSystem, Locator, Main }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
-  const file1 = `${tmpDir}/file1.ts`
-  const file2 = `${tmpDir}/file2.ts`
-  const file3 = `${tmpDir}/file3.ts`
+  const file1 = `${tmpDir}/file1.txt`
+  const file2 = `${tmpDir}/file2.txt`
+  const file3 = `${tmpDir}/file3.txt`
   await FileSystem.writeFile(file1, 'content1')
   await FileSystem.writeFile(file2, 'content2')
   await FileSystem.writeFile(file3, 'content3')
   await Main.openUri(file1)
   await Main.openUri(file2)
   await Main.openUri(file3)
-  const tab1 = Locator('.MainTab[title$="file1.ts"]')
 
   // act
-  await tab1.click()
+  await Command.execute('Main.selectTab', 0, 0)
 
   // assert
-  const selectedTab1 = Locator('.MainTabSelected[title$="file1.ts"]')
+  const selectedTab1 = Locator('.MainTabSelected[title$="file1.txt"]')
   await expect(selectedTab1).toBeVisible()
 }

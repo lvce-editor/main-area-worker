8f5e1a8f4d533afd64230658f250a5b45a7ec456
Le Vivilet <levivilet@pm.me>
2026-02-03 11:21:18 +0100

    e2e

--- packages/e2e/src/viewlet.main-area-auto-modified-status.ts
+++ packages/e2e/src/viewlet.main-area-auto-modified-status.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-auto-modified-status'
 
-// export const skip = 1
+export const skip = 1
 
 export const test: Test = async ({ Editor, expect, FileSystem, Locator, Main, Workspace }) => {
   // arrange

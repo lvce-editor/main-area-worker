dfaafd5c93434d9af576ef28f7e670b887ea037c
Le Vivilet <levivilet@pm.me>
2026-02-04 13:25:06 +0100

    feature: update handleTabContextMenu to include button parameter

--- packages/main-area-worker/src/parts/HandleTabContextMenu/HandleTabContextMenu.ts
+++ packages/main-area-worker/src/parts/HandleTabContextMenu/HandleTabContextMenu.ts
@@ -3,7 +3,7 @@ import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
 
-export const handleTabContextMenu = async (state: MainAreaState, x: number, y: number): Promise<MainAreaState> => {
+export const handleTabContextMenu = async (state: MainAreaState, button: number, x: number, y: number): Promise<MainAreaState> => {
   Assert.number(x)
   Assert.number(y)
   const { uid } = state

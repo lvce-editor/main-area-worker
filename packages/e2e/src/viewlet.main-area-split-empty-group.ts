9b6cd823da3bfbbc62fd01e4bc47ad3e1fe59aee
Le Vivilet <levivilet@pm.me>
2026-02-04 01:40:04 +0100

    feature: refactor test to use Main.splitRight for splitting editor groups (#273)

--- packages/e2e/src/viewlet.main-area-split-empty-group.ts
+++ packages/e2e/src/viewlet.main-area-split-empty-group.ts
@@ -2,11 +2,11 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-split-empty-group'
 
-export const test: Test = async ({ Command, expect, Locator }) => {
+export const test: Test = async ({ expect, Locator, Main }) => {
   // arrange
 
   // act
-  await Command.execute('Main.splitRight')
+  await Main.splitRight()
 
   // TODO: Add assertions for split functionality
   const editorGroups = Locator('.EditorGroup')

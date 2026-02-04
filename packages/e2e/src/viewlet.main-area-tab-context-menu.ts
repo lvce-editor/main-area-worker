830818b26c4e5c44c7ffbeb8e32d6b99daed4f48
Le Vivilet <levivilet@pm.me>
2026-02-04 13:30:08 +0100

    skip

--- packages/e2e/src/viewlet.main-area-tab-context-menu.ts
+++ packages/e2e/src/viewlet.main-area-tab-context-menu.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-tab-context-menu'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, FileSystem, Locator, Main }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()

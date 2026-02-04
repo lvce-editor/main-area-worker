795b2793ca1af775262cf7c096ff0c4a6cdf4faa
Le Vivilet <levivilet@pm.me>
2026-02-04 17:41:48 +0100

    fix

--- packages/main-area-worker/src/parts/HandleResize/HandleResize.ts
+++ packages/main-area-worker/src/parts/HandleResize/HandleResize.ts
@@ -3,9 +3,10 @@ import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
 import * as Assert from '../Assert/Assert.ts'
 
 export const handleResize = async (state: MainAreaState, dimensions: any): Promise<readonly any[]> => {
-  console.log({ dimensions })
   const { height, width, x, y } = dimensions
 
+  // TODO should update our local state also
+  // @ts-ignore
   const newState = {
     ...state,
     height,
@@ -23,13 +24,11 @@ export const handleResize = async (state: MainAreaState, dimensions: any): Promi
   for (const group of groups) {
     for (const tab of group.tabs) {
       if (tab.editorUid !== -1) {
-        console.log('editorid', tab.editorUid)
         const resizeCommands = await RendererWorker.invoke('Viewlet.resize', tab.editorUid, { height: contentHeight, width, x, y: y + tabHeight })
         allResizeCommands.push(...resizeCommands)
       }
     }
   }
 
-  console.log({ allResizeCommands })
   return allResizeCommands
 }

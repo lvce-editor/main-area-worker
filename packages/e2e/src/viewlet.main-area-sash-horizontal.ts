adbdd008f9acf6fa5fdd62d246ad4220b07afc56
Le Vivilet <levivilet@pm.me>
2026-02-20 15:12:34 +0100

    feature: add tests for horizontal and vertical sash functionality

---
+++ packages/e2e/src/viewlet.main-area-sash-horizontal.ts
@@ -0,0 +1,20 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-sash-horizontal'
+
+export const skip=1
+export const test: Test = async ({ Command, expect, FileSystem, Locator, Main }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  const file1 = `${tmpDir}/file1.ts`
+  const file2 = `${tmpDir}/file2.ts`
+
+  await FileSystem.writeFile(file1, 'content1')
+  await FileSystem.writeFile(file2, 'content2')
+
+  await Main.openUri(file1)
+  await Command.execute('Main.splitRight')
+
+  const sash = Locator('.SashHorizontal')
+  await expect(sash).toBeVisible()
+  await expect(sash).toHaveCount(1)
+}
---
+++ packages/e2e/src/viewlet.main-area-sash-vertical.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-sash-vertical'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, expect, FileSystem, Locator, Main }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  const file1 = `${tmpDir}/file1.ts`
+  const file2 = `${tmpDir}/file2.ts`
+
+  await FileSystem.writeFile(file1, 'content1')
+  await FileSystem.writeFile(file2, 'content2')
+
+  await Main.openUri(file1)
+  await Command.execute('Main.splitDown')
+
+  const sash = Locator('.SashVertical')
+  await expect(sash).toBeVisible()
+  await expect(sash).toHaveCount(1)
+}

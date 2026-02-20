2f986f2d5d302ee2f1fcf9b6616426a193e0a25a
Le Vivilet <levivilet@pm.me>
2026-02-20 17:35:32 +0100

    fix

--- packages/e2e/src/viewlet.main-area-editor-type-character.ts
+++ packages/e2e/src/viewlet.main-area-editor-type-character.ts
@@ -2,13 +2,18 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-editor-type-character'
 
-export const test: Test = async ({ Editor, FileSystem, Main }) => {
+export const test: Test = async ({ Editor, FileSystem, Main, Workspace }) => {
+  // arrange
   const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
   const testFile = `${tmpDir}/new-file.ts`
-
   await FileSystem.writeFile(testFile, '\n')
   await Main.openUri(testFile)
+  await Editor.setCursor(0, 0)
 
+  // act
   await Editor.type('a')
-  await Editor.shouldHaveText('a')
+
+  // assert
+  await Editor.shouldHaveText('a\n')
 }

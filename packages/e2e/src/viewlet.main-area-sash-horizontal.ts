e5357f2685a01cca92e60e95b2d5e03f5da7b63b
Le Vivilet <levivilet@pm.me>
2026-02-20 15:12:44 +0100

    fix

--- packages/e2e/src/viewlet.main-area-sash-horizontal.ts
+++ packages/e2e/src/viewlet.main-area-sash-horizontal.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-sash-horizontal'
 
-export const skip=1
+export const skip = 1
 export const test: Test = async ({ Command, expect, FileSystem, Locator, Main }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const file1 = `${tmpDir}/file1.ts`

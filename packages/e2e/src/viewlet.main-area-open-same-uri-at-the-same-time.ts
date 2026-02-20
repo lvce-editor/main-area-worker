2f51e95073db17729986a0c443cf059dcec8564a
Le Vivilet <levivilet@pm.me>
2026-02-20 17:36:54 +0100

    fix

--- packages/e2e/src/viewlet.main-area-open-same-uri-at-the-same-time.ts
+++ packages/e2e/src/viewlet.main-area-open-same-uri-at-the-same-time.ts
@@ -2,9 +2,12 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-open-same-uri-at-the-same-time'
 
-export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
+export const skip = 1
+
+export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
   const testFile = `${tmpDir}/same-file.txt`
   await FileSystem.writeFile(testFile, 'content')
 

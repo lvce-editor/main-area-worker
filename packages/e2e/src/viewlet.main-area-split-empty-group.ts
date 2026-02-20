7371bd4c8155a444e22104bb75da4c851ef03553
Le Vivilet <levivilet@pm.me>
2026-02-21 00:40:00 +0100

    feat: enhance test setup in viewlet.main-area-split-empty-group to include FileSystem and Workspace handling

--- packages/e2e/src/viewlet.main-area-split-empty-group.ts
+++ packages/e2e/src/viewlet.main-area-split-empty-group.ts
@@ -2,7 +2,10 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-split-empty-group'
 
-export const test: Test = async ({ expect, Locator, Main }) => {
+export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
+
   // act
   await Main.splitRight()
 

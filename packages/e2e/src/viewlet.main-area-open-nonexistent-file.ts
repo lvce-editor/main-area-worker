6964b3af4b666f69512749d09d283242ecf98586
Le Vivilet <levivilet@pm.me>
2026-02-20 16:03:57 +0100

    skip

--- packages/e2e/src/viewlet.main-area-open-nonexistent-file.ts
+++ packages/e2e/src/viewlet.main-area-open-nonexistent-file.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-open-nonexistent-file'
 
+export const skip = 1
+
 export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const nonExistentFile = `${tmpDir}/does-not-exist.ts`

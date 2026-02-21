26d82d16c371a84e3602faca182536f337dd5dcf
Le Vivilet <levivilet@pm.me>
2026-02-21 14:55:33 +0100

    feat: add tests for splitting editor down 100 and 10,000 times (#378)

---
+++ packages/e2e/src/viewlet.main-area-split-down-10000-times.ts
@@ -0,0 +1,24 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-split-down-10000-times'
+
+export const skip = 1
+
+const splitCount = 10_000
+
+export const test: Test = async ({ Command, expect, FileSystem, Locator, Main, Workspace }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
+  const file1 = `${tmpDir}/file1.ts`
+
+  await FileSystem.writeFile(file1, 'content1')
+
+  await Main.openUri(file1)
+
+  for (let i = 0; i < splitCount; i++) {
+    await Command.execute('Main.splitDown')
+  }
+
+  const editorGroups = Locator('.EditorGroup')
+  await expect(editorGroups).toHaveCount(splitCount + 1)
+}

ad9c2440d7d6f0746211a5589c7a6fa32be4621d
Le Vivilet <levivilet@pm.me>
2026-02-21 01:10:36 +0100

    feat: update renderSash to return an array of nodes and adjust related tests for border rendering

--- packages/main-area-worker/test/RenderSash.test.ts
+++ packages/main-area-worker/test/RenderSash.test.ts
@@ -4,20 +4,32 @@ import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctio
 import { renderSash } from '../src/parts/RenderSash/RenderSash.ts'
 
 test('renderSash vertical', () => {
-  const node = renderSash('vertical', '1:2', 'top:50%;')
+  const nodes = renderSash('vertical', '1:2', 'top:50%;')
+  const node = nodes[0]
+  const border = nodes[1]
   expect(node.className).toBe('Sash SashHorizontal')
   expect(node['data-sashId']).toBe('1:2')
+  expect(node.childCount).toBe(1)
   expect(node.onPointerDown).toBe(DomEventListenerFunctions.HandleSashPointerDown)
   expect(node.onPointerMove).toBe(DomEventListenerFunctions.HandleSashPointerMove)
   expect(node.onPointerUp).toBe(DomEventListenerFunctions.HandleSashPointerUp)
   expect(node.style).toBe('top:50%;')
   expect(node.type).toBe(VirtualDomElements.Div)
+  expect(border.className).toBe('SashBorder')
+  expect(border.childCount).toBe(0)
+  expect(border.type).toBe(VirtualDomElements.Div)
 })
 
 test('renderSash horizontal', () => {
-  const node = renderSash('horizontal', '1:2', 'left:50%;')
+  const nodes = renderSash('horizontal', '1:2', 'left:50%;')
+  const node = nodes[0]
+  const border = nodes[1]
   expect(node.className).toBe('Sash SashVertical')
   expect(node['data-sashId']).toBe('1:2')
+  expect(node.childCount).toBe(1)
   expect(node.style).toBe('left:50%;')
   expect(node.type).toBe(VirtualDomElements.Div)
+  expect(border.className).toBe('SashBorder')
+  expect(border.childCount).toBe(0)
+  expect(border.type).toBe(VirtualDomElements.Div)
 })

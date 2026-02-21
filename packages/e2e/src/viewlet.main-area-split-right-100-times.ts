229d623d90414ccaff82adaea49928b5b5d9f061
Le Vivilet <levivilet@pm.me>
2026-02-21 14:50:28 +0100

    feat: add test for splitting editor area 100 times (#377)

---
+++ packages/e2e/src/viewlet.main-area-split-right-100-times.ts
@@ -0,0 +1,22 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-split-right-100-times'
+
+const splitCount = 100
+
+export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  await Workspace.setPath(tmpDir)
+  const file1 = `${tmpDir}/file1.ts`
+
+  await FileSystem.writeFile(file1, 'content1')
+
+  await Main.openUri(file1)
+
+  for (let i = 0; i < splitCount; i++) {
+    await Main.splitRight()
+  }
+
+  const editorGroups = Locator('.EditorGroup')
+  await expect(editorGroups).toHaveCount(splitCount + 1)
+}

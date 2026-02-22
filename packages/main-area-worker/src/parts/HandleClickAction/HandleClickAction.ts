7691fd721d56453ebad73b6c47c4fa06e47b2cf4
Le Vivilet <levivilet@pm.me>
2026-02-22 10:59:16 +0100

    refactor: replace string literals with constants from InputName for better maintainability (#404)

--- packages/main-area-worker/src/parts/HandleClickAction/HandleClickAction.ts
+++ packages/main-area-worker/src/parts/HandleClickAction/HandleClickAction.ts
@@ -3,6 +3,7 @@ import { closeEditorGroup } from '../CloseEditorGroup/CloseEditorGroup.ts'
 import { getActiveGroup } from '../GetActiveGroup/GetActiveGroup.ts'
 import * as GroupDirection from '../GroupDirection/GroupDirection.ts'
 import { handleClickTogglePreview } from '../HandleClickTogglePreview/HandleClickTogglePreview.ts'
+import * as InputName from '../InputName/InputName.ts'
 import { retryOpen } from '../RetryOpen/RetryOpen.ts'
 import { splitEditorGroup } from '../SplitEditorGroup/SplitEditorGroup.ts'
 
@@ -23,17 +24,17 @@ export const handleClickAction = async (state: MainAreaState, action: string, ra
   }
 
   switch (action) {
-    case 'close-group':
+    case InputName.CloseGroup:
       if (rawGroupId) {
         const groupId = Number.parseInt(rawGroupId, 10)
         return closeEditorGroup(state, groupId)
       }
       return state
-    case 'retry-open':
+    case InputName.RetryOpen:
       return retryOpen(state)
-    case 'split-right':
+    case InputName.SplitRight:
       return splitEditorGroup(state, activeGroup.id, GroupDirection.Right)
-    case 'toggle-preview':
+    case InputName.TogglePreview:
       return handleClickTogglePreview(state)
     default:
       return state

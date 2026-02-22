a97da3269fbbe4d555085ab2466c6357fd042dee
Le Vivilet <levivilet@pm.me>
2026-02-22 10:35:28 +0100

    refactor: update TODO comment for clipboard worker usage in copyRelativePath function

--- packages/main-area-worker/src/parts/CopyRelativePath/CopyRelativePath.ts
+++ packages/main-area-worker/src/parts/CopyRelativePath/CopyRelativePath.ts
@@ -4,6 +4,7 @@ import * as Assert from '../Assert/Assert.ts'
 
 export const copyRelativePath = async (state: MainAreaState, path: string): Promise<MainAreaState> => {
   Assert.string(path)
+  // TODO use clipboard worker
   const relativePath = await RendererWorker.invoke('Workspace.pathBaseName', path)
   await RendererWorker.invoke('ClipBoard.writeText', relativePath)
   return state

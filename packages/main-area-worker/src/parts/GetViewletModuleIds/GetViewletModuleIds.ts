1906eb859dcd90d466059414a193a31b1038460c
Le Vivilet <levivilet@pm.me>
2026-02-22 10:29:59 +0100

    refactor: improve type safety and readability in getViewletModuleIds function (#393)

--- packages/main-area-worker/src/parts/GetViewletModuleIds/GetViewletModuleIds.ts
+++ packages/main-area-worker/src/parts/GetViewletModuleIds/GetViewletModuleIds.ts
@@ -1,11 +1,13 @@
+import type { MainAreaLayout } from '../MainAreaLayout/MainAreaLayout.ts'
 import type { Tab } from '../Tab/Tab.ts'
 import { getViewletModuleId } from '../GetViewletModuleId/GetViewletModuleId.ts'
 
-export const getViewletModuleIds = async (layout: any): Promise<Record<string, string>> => {
+export const getViewletModuleIds = async (layout: MainAreaLayout): Promise<Record<string, string>> => {
   const viewletModuleIds: Record<string, string> = {}
 
   for (const group of layout.groups) {
-    const activeTab = group.tabs.find((tab: Tab) => tab.id === group.activeTabId)
+    const { tabs } = group
+    const activeTab = tabs.find((tab: Tab) => tab.id === group.activeTabId)
     if (activeTab && activeTab.uri) {
       const viewletModuleId = await getViewletModuleId(activeTab.uri)
       if (viewletModuleId) {

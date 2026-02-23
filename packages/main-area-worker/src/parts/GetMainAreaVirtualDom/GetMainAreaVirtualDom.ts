9913e96009189066454b8fe786788ba109eabc07
Le Vivilet <levivilet@pm.me>
2026-02-23 13:36:57 +0100

    fix

--- packages/main-area-worker/src/parts/GetMainAreaVirtualDom/GetMainAreaVirtualDom.ts
+++ packages/main-area-worker/src/parts/GetMainAreaVirtualDom/GetMainAreaVirtualDom.ts
@@ -20,21 +20,14 @@ export const getMainAreaVirtualDom = (layout: MainAreaLayout, splitButtonEnabled
     ? `${ClassNames.EDITOR_GROUPS_CONTAINER} ${directionClassName}`
     : ClassNames.EDITOR_GROUPS_CONTAINER
   let sashOffset = 0
-<<<<<<< HEAD
   let editorGroupsContainerChildCount = 0
   for (let i = 0; i < layout.groups.length; i++) {
     sashOffset += layout.groups[i].size
-=======
-  let childCount = 0
-  for (let i = 0; i < groups.length; i++) {
-    sashOffset += groups[i].size
->>>>>>> origin/main
     if (i > 0) {
       // Insert sash between groups
       const beforeGroupId = groups[i - 1].id
       const afterGroupId = groups[i].id
       const sashId = SashId.create(beforeGroupId, afterGroupId)
-<<<<<<< HEAD
       const style = layout.direction === 'horizontal' ? `left:${sashOffset - layout.groups[i].size}%;` : `top:${sashOffset - layout.groups[i].size}%;`
       children.push(...renderSash(layout.direction, sashId, style))
       editorGroupsContainerChildCount++
@@ -42,14 +35,6 @@ export const getMainAreaVirtualDom = (layout: MainAreaLayout, splitButtonEnabled
     const editorGroupDom = renderEditorGroup(layout.groups[i], i, splitButtonEnabled, sizeProperty)
     children.push(...editorGroupDom)
     editorGroupsContainerChildCount++
-=======
-      const style = direction === 'horizontal' ? `left:${sashOffset - groups[i].size}%;` : `top:${sashOffset - groups[i].size}%;`
-      children.push(...renderSash(direction, sashId, style))
-      childCount++
-    }
-    children.push(...renderEditorGroup(groups[i], i, splitButtonEnabled, sizeProperty))
-    childCount++
->>>>>>> origin/main
   }
   return [
     {
@@ -58,11 +43,7 @@ export const getMainAreaVirtualDom = (layout: MainAreaLayout, splitButtonEnabled
       type: VirtualDomElements.Div,
     },
     {
-<<<<<<< HEAD
       childCount: editorGroupsContainerChildCount,
-=======
-      childCount,
->>>>>>> origin/main
       className: editorGroupsContainerClassName,
       role: AriaRoles.None,
       type: VirtualDomElements.Div,

5a3e1e5c2b72e7b8ffcc7e5ee10777357a93dd67
Le Vivilet <levivilet@pm.me>
2026-02-23 13:40:02 +0100

    fix

--- packages/main-area-worker/src/parts/RenderSash/RenderSash.ts
+++ packages/main-area-worker/src/parts/RenderSash/RenderSash.ts
@@ -1,15 +1,17 @@
 import { type VirtualDomNode, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
+const getSashClassName = (direction: 'horizontal' | 'vertical'): string => {
+  return direction === 'horizontal' ? 'Sash SashVertical' : 'Sash SashHorizontal'
+}
+
 export const renderSash = (direction: 'horizontal' | 'vertical', sashId: string, style: string): readonly VirtualDomNode[] => {
   return [
     {
       childCount: 1,
-      className: direction === 'horizontal' ? 'Sash SashVertical' : 'Sash SashHorizontal',
+      className: getSashClassName(direction),
       'data-sashId': sashId,
       onPointerDown: DomEventListenerFunctions.HandleSashPointerDown,
-      onPointerMove: DomEventListenerFunctions.HandleSashPointerMove,
-      onPointerUp: DomEventListenerFunctions.HandleSashPointerUp,
       style,
       type: VirtualDomElements.Div,
     },

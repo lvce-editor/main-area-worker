6556de5c1fa34b00232a9a1c268d9bada11a3db6
Le Vivilet <levivilet@pm.me>
2026-02-24 11:13:43 +0100

    fix: update error file path and tab title in open file error test

--- packages/e2e/src/viewlet.main-area-open-file-error.ts
+++ packages/e2e/src/viewlet.main-area-open-file-error.ts
@@ -2,27 +2,24 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-open-file-error'
 
-// export const skip = 1
-
 export const test: Test = async ({ expect, Extension, Locator, Main, Workspace }) => {
   // arrange
   const extensionUri = import.meta.resolve('../fixtures/read-file-error')
   await Extension.addWebExtension(extensionUri)
   const prefix = 'extension-host://xyz://'
   await Workspace.setPath(prefix)
-  const errorFile = `${prefix}/error-file.ts`
+  const errorFile = `${prefix}/test.txt`
 
   // act
   await Main.openUri(errorFile)
 
   // assert
-  const tab = Locator('.MainTab[title$="error-file.ts"]')
+  const tab = Locator('.MainTab[title$="test.txt"]')
   await expect(tab).toBeVisible()
-
   const errorContent = Locator('.EditorContentError')
   await expect(errorContent).toBeVisible()
-  // await expect(errorContent).toContainText('File not found')
-
+  // TODO improve error message
+  await expect(errorContent).toHaveText(`Error: cannot append child: instance -1 not found`)
   const retryButton = Locator('.EditorContentError .Button')
   await expect(retryButton).toBeVisible()
   await expect(retryButton).toHaveText('Retry')

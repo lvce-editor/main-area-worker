a12a60a6ac295c4b3c63e97fd206371fea095eca
Le Vivilet <levivilet@pm.me>
2026-02-24 10:59:13 +0100

    feat: refactor test setup to use extension URI and update file path handling (#417)

---
+++ packages/e2e/fixtures/read-file-error/main.js
@@ -0,0 +1,39 @@
+const contents = Object.create(null)
+
+contents['/test.txt'] = 'Hello WOrld'
+
+let i = 0
+
+const fileSystemProvider = {
+  id: 'xyz',
+  writeFile(uri, content) {
+    contents[uri] = content
+  },
+  rename(oldUri, newUri) {},
+  readFile(uri) {
+    if (i++ === 0) {
+      throw new Error(`File not found`)
+    }
+    return contents[uri]
+  },
+  pathSeparator: '/',
+  readDirWithFileTypes(uri) {
+    const results = []
+    for (const [key, value] of Object.entries(contents)) {
+      if (key.startsWith(uri)) {
+        results.push({
+          type: 7,
+          name: key.slice(key.lastIndexOf('/')),
+        })
+      }
+    }
+    return results
+  },
+  remove(uri) {
+    throw new Error(`oops`)
+  },
+}
+
+export const activate = () => {
+  vscode.registerFileSystemProvider(fileSystemProvider)
+}
--- packages/e2e/src/viewlet.main-area-open-file-error.ts
+++ packages/e2e/src/viewlet.main-area-open-file-error.ts
@@ -2,13 +2,15 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-open-file-error'
 
-export const skip = 1
+// export const skip = 1
 
-export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace }) => {
+export const test: Test = async ({ expect, Extension, Locator, Main, Workspace }) => {
   // arrange
-  const tmpDir = await FileSystem.getTmpDir()
-  await Workspace.setPath(tmpDir)
-  const errorFile = `${tmpDir}/error-file.ts`
+  const extensionUri = import.meta.resolve('../fixtures/read-file-error')
+  await Extension.addWebExtension(extensionUri)
+  const prefix = 'extension-host://xyz://'
+  await Workspace.setPath(prefix)
+  const errorFile = `${prefix}/error-file.ts`
 
   // act
   await Main.openUri(errorFile)
@@ -19,7 +21,7 @@ export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace 
 
   const errorContent = Locator('.EditorContent--error')
   await expect(errorContent).toBeVisible()
-  await expect(errorContent).toContainText('File not found')
+  // await expect(errorContent).toContainText('File not found')
 
   const retryButton = Locator('.EditorContent--error .Button')
   await expect(retryButton).toBeVisible()

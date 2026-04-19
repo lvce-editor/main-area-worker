2d4d1c48623377186ce381b159c506a3bc4821f4
Le Vivilet <levivilet@pm.me>
2026-04-19 08:21:02 +0200

    fix

--- packages/main-area-worker/src/parts/RefreshFileIcons/RefreshFileIcons.ts
+++ packages/main-area-worker/src/parts/RefreshFileIcons/RefreshFileIcons.ts
@@ -1,10 +1,10 @@
-import * as MainAreaStates from '../MainAreaStates/MainAreaStates.ts'
 import { handleIconThemeChange } from '../HandleIconThemeChange/HandleIconThemeChange.ts'
+import * as MainAreaStates from '../MainAreaStates/MainAreaStates.ts'
 
 export const refreshFileIcons = async (): Promise<void> => {
   const keys = MainAreaStates.getKeys()
   for (const uid of keys) {
-    const { oldState, newState } = MainAreaStates.get(uid)
+    const { newState, oldState } = MainAreaStates.get(uid)
     const updatedState = await handleIconThemeChange(newState)
     MainAreaStates.set(uid, oldState, updatedState)
   }

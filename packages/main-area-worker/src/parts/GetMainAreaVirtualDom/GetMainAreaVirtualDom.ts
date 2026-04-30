957a0282a63ef16ea96ed68fd704bc70925cf31f
Le Vivilet <levivilet@pm.me>
2026-04-30 16:04:09 +0200

    fix

--- packages/main-area-worker/src/parts/GetMainAreaVirtualDom/GetMainAreaVirtualDom.ts
+++ packages/main-area-worker/src/parts/GetMainAreaVirtualDom/GetMainAreaVirtualDom.ts
@@ -1,32 +1,12 @@
 import { type VirtualDomNode, AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { MainAreaLayout } from '../MainAreaState/MainAreaState.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { getSashOffset } from '../GetSashOffset/GetSashOffset.ts'
 import * as LayoutDirection from '../LayoutDirection/LayoutDirection.ts'
 import { renderEditorGroup } from '../RenderEditorGroup/RenderEditorGroup.ts'
 import { renderSash } from '../RenderSash/RenderSash.ts'
 import { renderSingleEditorGroup } from '../RenderSingleEditorGroup/RenderSingleEditorGroup.ts'
 import * as SashId from '../SashId/SashId.ts'
-
-const MIN_GROUP_WIDTH_PX = 250
-
-const getSashOffset = (layout: MainAreaLayout, groupIndex: number, width: number): string => {
-  const { direction, groups } = layout
-  const percentOffset = groups.slice(0, groupIndex).reduce((total, group) => total + group.size, 0)
-
-  if (direction !== LayoutDirection.Horizontal || !width || !Number.isFinite(width)) {
-    return `${percentOffset}%`
-  }
-
-  const effectiveGroupSizes = groups.map((group) => Math.max((group.size / 100) * width, MIN_GROUP_WIDTH_PX))
-  const hasOverflowingGroups = effectiveGroupSizes.some((size, index) => size !== (groups[index].size / 100) * width)
-
-  if (!hasOverflowingGroups) {
-    return `${percentOffset}%`
-  }
-
-  const pixelOffset = effectiveGroupSizes.slice(0, groupIndex).reduce((total, size) => total + size, 0)
-  return `${pixelOffset}px`
-}
 
 export const getMainAreaVirtualDom = (layout: MainAreaLayout, splitButtonEnabled: boolean = false, width: number = 0): readonly VirtualDomNode[] => {
   const { direction, groups } = layout
@@ -45,10 +25,8 @@ export const getMainAreaVirtualDom = (layout: MainAreaLayout, splitButtonEnabled
   const editorGroupsContainerClassName = directionClassName
     ? `${ClassNames.EDITOR_GROUPS_CONTAINER} ${directionClassName}`
     : ClassNames.EDITOR_GROUPS_CONTAINER
-  let sashOffset = 0
   let childCount = 0
   for (let i = 0; i < groups.length; i++) {
-    sashOffset += groups[i].size
     if (i > 0) {
       // Insert sash between groups
       const beforeGroupId = groups[i - 1].id

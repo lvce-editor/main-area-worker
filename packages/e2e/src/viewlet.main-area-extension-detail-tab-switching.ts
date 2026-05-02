6743dda28b6cb1f06558604bd723f29e36d14111
Le Vivilet <levivilet@pm.me>
2026-05-02 12:47:46 +0000

    feature: add end-to-end test for main area extension detail tab switching

---
+++ packages/e2e/src/viewlet.main-area-extension-detail-tab-switching.ts
@@ -0,0 +1,24 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-extension-detail-tab-switching'
+
+export const test: Test = async ({ expect, Locator, Main }) => {
+  // arrange
+  await Main.openUri('extension-detail://theme-ayu')
+  await Main.openUri('extension-detail://chat')
+
+  const themeAyuTab = Locator('.MainTab[title="theme-ayu"]')
+  const chatTab = Locator('.MainTab[title="chat"]')
+  await expect(themeAyuTab).toBeVisible()
+  await expect(chatTab).toBeVisible()
+
+  const selectedChatTab = Locator('.MainTabSelected[title="chat"]')
+  await expect(selectedChatTab).toBeVisible()
+
+  // act
+  await Main.selectTab(0, 0)
+
+  // assert
+  const selectedThemeAyuTab = Locator('.MainTabSelected[title="theme-ayu"]')
+  await expect(selectedThemeAyuTab).toBeVisible()
+}

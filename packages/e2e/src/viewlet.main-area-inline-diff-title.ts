0d61cda89afeda4049c1b197028aecc67855b2dd
Le Vivilet <levivilet@pm.me>
2026-05-03 20:29:49 +0200

    feature: improve test readability by adjusting formatting in inline diff title test (#481)

--- packages/e2e/src/viewlet.main-area-inline-diff-title.ts
+++ packages/e2e/src/viewlet.main-area-inline-diff-title.ts
@@ -3,6 +3,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 export const name = 'viewlet.main-area-inline-diff-title'
 
 export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
+  await Main.closeAllEditors()
   const tmpDir = await FileSystem.getTmpDir()
   const workingTreeFile = `${tmpDir}/inline-diff-title.txt`
 

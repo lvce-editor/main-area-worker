62853b1d2c918cd4ccfa5d20fefc6e4cc97c196b
Le Vivilet <levivilet@pm.me>
2026-05-04 10:12:46 +0000

    fix

--- packages/main-area-worker/src/parts/GetFilteredGroups/GetFilteredGroups.ts
+++ packages/main-area-worker/src/parts/GetFilteredGroups/GetFilteredGroups.ts
@@ -2,15 +2,14 @@ import type { EditorGroup } from '../EditorGroup/EditorGroup.ts'
 import { normalizeTabEditorInput } from '../NormalizeTabEditorInput/NormalizeTabEditorInput.ts'
 
 export const getFilteredGroups = (groups: readonly EditorGroup[]): readonly EditorGroup[] => {
-  return groups
-    .map((group) => {
-      const tabs = group.tabs.filter((tab) => !tab.uri?.startsWith('untitled://')).map(normalizeTabEditorInput)
-      const activeTabId = tabs.some((tab) => tab.id === group.activeTabId) ? group.activeTabId : undefined
-      return {
-        ...group,
-        activeTabId,
-        isEmpty: tabs.length === 0,
-        tabs,
-      }
-    })
+  return groups.map((group) => {
+    const tabs = group.tabs.filter((tab) => !tab.uri?.startsWith('untitled://')).map(normalizeTabEditorInput)
+    const activeTabId = tabs.some((tab) => tab.id === group.activeTabId) ? group.activeTabId : undefined
+    return {
+      ...group,
+      activeTabId,
+      isEmpty: tabs.length === 0,
+      tabs,
+    }
+  })
 }

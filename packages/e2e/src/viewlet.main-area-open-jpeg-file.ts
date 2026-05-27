8cfedbcc5da95f8ee0176e18cfd5267394855935
Le Vivilet <levivilet@pm.me>
2026-05-27 18:32:00 +0200

    feature: add tests for opening various image file types in the viewlet (#514)

---
+++ packages/e2e/src/viewlet.main-area-open-jpeg-file.ts
@@ -0,0 +1,17 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-open-jpeg-file'
+
+export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  const testFile = `${tmpDir}/test.jpeg`
+  await FileSystem.writeFile(testFile, '')
+
+  await Main.openUri(testFile)
+
+  const tab = Locator('.MainTab[title$="test.jpeg"]')
+  await expect(tab).toBeVisible()
+
+  const image = Locator('img').first()
+  await expect(image).toBeVisible()
+}

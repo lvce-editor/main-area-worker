257fff835868d963ddfc06e81a12a98a657737a8
Le Vivilet <levivilet@pm.me>
2026-05-27 21:25:04 +0200

    fix

--- packages/e2e/src/viewlet.main-area-open-webp-file.ts
+++ packages/e2e/src/viewlet.main-area-open-webp-file.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-open-webp-file'
 
-export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
+export const test: Test = async ({ expect, Locator, Main }) => {
   const testFile = import.meta.resolve('../fixtures/media/tiny.webp')
 
   await Main.openUri(testFile)

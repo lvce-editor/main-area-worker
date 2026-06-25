e061ed1cdcc8bc2f3b9002ca5e9e2d163c971529
Le Vivilet <levivilet@pm.me>
2026-06-25 22:21:00 +0200

    Limit webkit e2e on windows

--- packages/e2e/scripts/run-webkit-e2e.js
+++ packages/e2e/scripts/run-webkit-e2e.js
@@ -9,7 +9,10 @@ const excludedTests = new Set([
   'viewlet.main-area-restore-closed-tab-three-groups.ts',
 ])
 
+const windowsTests = new Set(['viewlet.main-area-close-active-editor-no-tabs.ts', 'viewlet.main-area-close-active-editor-one-tab.ts'])
+
 const cwd = process.cwd()
+const isWindows = process.platform === 'win32'
 const tmpRoot = join(cwd, '.tmp')
 const tmpTestPath = join(tmpRoot, 'e2e-webkit')
 const tmpSourcePath = join(tmpTestPath, 'src')
@@ -23,7 +26,7 @@ const copyWebkitTests = async () => {
 
   const entries = await readdir(sourcePath, { withFileTypes: true })
   for (const entry of entries) {
-    if (!entry.isFile() || excludedTests.has(entry.name)) {
+    if (!entry.isFile() || excludedTests.has(entry.name) || (isWindows && !windowsTests.has(entry.name))) {
       continue
     }
     await cp(join(sourcePath, entry.name), join(tmpSourcePath, entry.name))

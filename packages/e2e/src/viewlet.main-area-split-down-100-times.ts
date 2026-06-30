89ebeecd4a8fae66eea7c3d72386ef18c575cfdc
Le Vivilet <levivilet@pm.me>
2026-06-30 16:54:07 +0200

    test: skip slow split down stress test

--- packages/e2e/src/viewlet.main-area-split-down-100-times.ts
+++ packages/e2e/src/viewlet.main-area-split-down-100-times.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-split-down-100-times'
 
+export const skip = 1
+
 const splitCount = 100
 
 export const test: Test = async ({ expect, FileSystem, Locator, Main, Workspace }) => {

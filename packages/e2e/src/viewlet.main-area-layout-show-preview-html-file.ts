9cc458249fa7dde8a845bb9dab9aa09cc4321907
Le Vivilet <levivilet@pm.me>
2026-07-05 09:10:25 +0200

    skip

--- packages/e2e/src/viewlet.main-area-layout-show-preview-html-file.ts
+++ packages/e2e/src/viewlet.main-area-layout-show-preview-html-file.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-layout-show-preview-html-file'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, FileSystem, Locator }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const htmlFile = `${tmpDir}/test.html`

4d9886a721f58a37ea4aa53f9d561f602319903e
Le Vivilet <levivilet@pm.me>
2026-07-10 09:30:00 +0000

    test: skip unavailable process explorer e2e

--- packages/e2e/src/viewlet.main-area-process-explorer-tab-switching.ts
+++ packages/e2e/src/viewlet.main-area-process-explorer-tab-switching.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-process-explorer-tab-switching'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, FileSystem, Locator, Main }) => {
   // arrange
   await Command.execute('Developer.openProcessExplorer')

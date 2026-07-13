ebf93e87e5a47efd33700d3ffa5565d58f062cc1
Le Vivilet <levivilet@pm.me>
2026-07-13 11:46:40 +0200

    feature: restore main area state per workspace (#581)

--- packages/main-area-worker/src/parts/HandleWorkspaceChange/HandleWorkspaceChange.ts
+++ packages/main-area-worker/src/parts/HandleWorkspaceChange/HandleWorkspaceChange.ts
@@ -1,13 +1,6 @@
 import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
+import * as LoadContent from '../LoadContent/LoadContent.ts'
 
-export const handleWorkspaceChange = (state: MainAreaState): MainAreaState => {
-  const { layout } = state
-  return {
-    ...state,
-    layout: {
-      ...layout,
-      activeGroupId: undefined,
-      groups: [],
-    },
-  }
+export const handleWorkspaceChange = async (state: MainAreaState, _workspacePath?: string, savedState?: unknown): Promise<MainAreaState> => {
+  return LoadContent.loadContent(state, savedState)
 }

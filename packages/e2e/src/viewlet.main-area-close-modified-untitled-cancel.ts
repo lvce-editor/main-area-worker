ff3d437272e936c336a42959fecc9326f3e65aa8
Le Vivilet <levivilet@pm.me>
2026-07-16 13:02:30 +0000

    fix: skip unsupported webkit untitled e2e

--- packages/e2e/src/viewlet.main-area-close-modified-untitled-cancel.ts
+++ packages/e2e/src/viewlet.main-area-close-modified-untitled-cancel.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-close-modified-untitled-cancel'
 
+export const skip = ['webkit']
+
 export const test: Test = async ({ Command, Editor, expect, Locator, Main }) => {
   await Command.execute('Main.newFile')
 

3d1ed6c6283e8d7da3af257b5ac0544ebe79edee
Le Vivilet <levivilet@pm.me>
2026-07-16 14:10:45 +0000

    test: skip direct pinning helper on webkit

--- packages/e2e/src/viewlet.main-area-preview-tab-open-non-preview-pins.ts
+++ packages/e2e/src/viewlet.main-area-preview-tab-open-non-preview-pins.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-preview-tab-open-non-preview-pins'
 
+export const skip = ['webkit'] as const
+
 export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
   const tmpDir = await FileSystem.getTmpDir()
   const firstFile = `${tmpDir}/pin-existing-first.ts`

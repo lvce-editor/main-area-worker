777913bc3f75a3b5609594ec361cca013e050e2c
Le Vivilet <levivilet@pm.me>
2026-07-18 06:36:06 +0000

    fix: split empty group through layout menu

--- packages/e2e/src/viewlet.main-area-split-up-close-lower-group.ts
+++ packages/e2e/src/viewlet.main-area-split-up-close-lower-group.ts
@@ -1,14 +1,26 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'viewlet.main-area-split-up-close-lower-group'
+export const skip = ['webkit'] as const
 
 const clickEventInit = { bubbles: true } as unknown as string
 
-export const test: Test = async ({ Command, expect, FileSystem, Locator, Workspace }) => {
+export const test: Test = async ({ expect, FileSystem, Locator, Main, TitleBarMenuBar, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await Workspace.setPath(tmpDir)
+  await Main.closeAllEditors()
 
-  await Command.execute('MainArea.splitUp')
+  await TitleBarMenuBar.focus()
+  await TitleBarMenuBar.handleKeyArrowRight()
+  await TitleBarMenuBar.handleKeyArrowRight()
+  await TitleBarMenuBar.handleKeyArrowRight()
+  await TitleBarMenuBar.handleKeyArrowDown()
+  const editorLayoutMenuItem = Locator('#Menu-0 .MenuItem', { hasText: 'Editor Layout' })
+  await expect(editorLayoutMenuItem).toBeVisible()
+  await editorLayoutMenuItem.dispatchEvent('click', clickEventInit)
+  const splitUpMenuItem = Locator('#Menu-1 .MenuItem', { hasText: 'Split Up' })
+  await expect(splitUpMenuItem).toBeVisible()
+  await splitUpMenuItem.dispatchEvent('click', clickEventInit)
 
   const editorGroups = Locator('.EditorGroup')
   const closeButtons = Locator('.EmptyGroupCloseButton')

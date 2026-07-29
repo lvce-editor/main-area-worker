f377bfefe5f94a5bb40eb2bd9da6b0d0b92d88e3
Le Vivilet <levivilet@pm.me>
2026-07-29 17:05:45 +0200

    feature: simplify eslint config (#652)

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,40 @@
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
   ...config.recommendedVirtualDom,
+  ...config.recommendedActions,
+  ...config.recommendedTsconfig,
   ...config.recommendedRegex,
-  ...actions.default,
   {
     rules: {
       '@cspell/spellchecker': 'off',
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+  {
+    files: ['packages/main-area-worker/src/**/*.ts'],
+    rules: {
+      'virtual-dom/prefer-state-destructuring': 'off',
+    },
+  },
+  {
+    files: ['packages/e2e/tsconfig.json'],
+    rules: {
+      'tsconfig/allow-importing-ts-extensions': 'off',
+      'tsconfig/dont-skip-lib-check': 'off',
+      'tsconfig/exact-optional-property-types': 'off',
+      'tsconfig/force-consistent-casing-in-file-names': 'off',
+      'tsconfig/no-implicit-any': 'off',
+      'tsconfig/no-unchecked-side-effect-imports': 'off',
+    },
+  },
+  {
+    files: ['packages/main-area-worker/tsconfig.json'],
+    rules: {
+      'tsconfig/dont-skip-lib-check': 'off',
+      'tsconfig/exact-optional-property-types': 'off',
     },
   },
   {

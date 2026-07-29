44a915ebe3f6961443cea8539604f45d681375b1
Le Vivilet <levivilet@pm.me>
2026-07-29 18:00:11 +0000

    fix: replace webkit incompatible close scenarios

--- packages/e2e/src/viewlet.main-area-select-second-of-five-tabs.ts
+++ packages/e2e/src/viewlet.main-area-select-second-of-five-tabs.ts
@@ -1,18 +1,17 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const name = 'viewlet.main-area-close-active-after-select-first-of-six-tabs'
+export const name = 'viewlet.main-area-select-second-of-five-tabs'
 
 export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
   const tmpDir = await FileSystem.getTmpDir()
-  const files = Array.from({ length: 6 }, (_, index) => `${tmpDir}/close-active-select-first-${index + 1}.ts`)
+  const files = Array.from({ length: 5 }, (_, index) => `${tmpDir}/select-five-second-${index + 1}.ts`)
   await FileSystem.setFiles(files.map((uri, index) => ({ content: `export const value = ${index}`, uri })))
   await Main.openUris(files)
-  await Main.selectTab(0, 0)
 
-  await Main.closeActiveEditor()
+  await Main.selectTab(0, 1)
 
-  const closedTab = Locator('.MainTab[title$="close-active-select-first-1.ts"]')
+  const selectedTab = Locator('.MainTabSelected[title$="select-five-second-2.ts"]')
   const tabs = Locator('.MainTab')
-  await expect(closedTab).toBeHidden()
+  await expect(selectedTab).toBeVisible()
   await expect(tabs).toHaveCount(5)
 }

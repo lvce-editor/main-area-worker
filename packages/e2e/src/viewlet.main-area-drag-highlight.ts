51daef1d02e408a1bdab4ffd5d75aa5b78788d3b
Le Vivilet <levivilet@pm.me>
2026-07-30 09:08:53 +0000

    test: follow locator lint convention

--- packages/e2e/src/viewlet.main-area-drag-highlight.ts
+++ packages/e2e/src/viewlet.main-area-drag-highlight.ts
@@ -5,11 +5,13 @@ export const name = 'viewlet.main-area-drag-highlight'
 export const test: Test = async ({ expect, Locator }) => {
   const main = Locator('.Main')
   const dragOverlay = Locator('.DragOverlay')
+  const mainDragOverlay = Locator('.Main > .DragOverlay')
+  const nestedDragOverlay = Locator('.editor-groups-container .DragOverlay')
 
   await main.dispatchEvent('dragover', { bubbles: true, clientX: 500, clientY: 300 } as any)
   await expect(dragOverlay).toBeVisible()
-  await expect(Locator('.Main > .DragOverlay')).toHaveCount(1)
-  await expect(Locator('.editor-groups-container .DragOverlay')).toHaveCount(0)
+  await expect(mainDragOverlay).toHaveCount(1)
+  await expect(nestedDragOverlay).toHaveCount(0)
 
   await main.dispatchEvent('dragleave', { bubbles: true } as any)
   await expect(dragOverlay).toBeHidden()

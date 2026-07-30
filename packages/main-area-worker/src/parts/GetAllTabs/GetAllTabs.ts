7b6f3f2b706dfad21d120fc234695e6f5f20f139
Le Vivilet <levivilet@pm.me>
2026-07-30 11:03:37 +0200

    feature: simplify getTabs

--- packages/main-area-worker/src/parts/GetAllTabs/GetAllTabs.ts
+++ packages/main-area-worker/src/parts/GetAllTabs/GetAllTabs.ts
@@ -1,7 +1,11 @@
-import type { MainAreaState, Tab } from '../MainAreaState/MainAreaState.ts'
+import type { EditorGroup, MainAreaState, Tab } from '../MainAreaState/MainAreaState.ts'
+
+const getTabs = (group: EditorGroup): readonly Tab[] => {
+  return group.tabs
+}
 
 export const getAllTabs = (state: MainAreaState): Tab[] => {
   const { layout } = state
   const { groups } = layout
-  return groups.flatMap((group) => group.tabs)
+  return groups.flatMap(getTabs)
 }

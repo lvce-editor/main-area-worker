221bfdf903260447fb7e18887f295a6984bd1162
Le Vivilet <levivilet@pm.me>
2026-07-30 09:02:54 +0000

    fix: prevent drag overlay scrollbars

--- packages/e2e/src/viewlet.main-area-drag-highlight.ts
+++ packages/e2e/src/viewlet.main-area-drag-highlight.ts
@@ -8,6 +8,8 @@ export const test: Test = async ({ expect, Locator }) => {
 
   await main.dispatchEvent('dragover', { bubbles: true, clientX: 500, clientY: 300 } as any)
   await expect(dragOverlay).toBeVisible()
+  await expect(Locator('.Main > .DragOverlay')).toHaveCount(1)
+  await expect(Locator('.editor-groups-container .DragOverlay')).toHaveCount(0)
 
   await main.dispatchEvent('dragleave', { bubbles: true } as any)
   await expect(dragOverlay).toBeHidden()

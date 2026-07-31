d8ebcb929208756536c6d617f601e8c424771477
Le Vivilet <levivilet@pm.me>
2026-07-31 19:47:15 +0200

    feature: simplify sash class

--- packages/main-area-worker/src/parts/GetSashClassName/GetSashClassName.ts
+++ packages/main-area-worker/src/parts/GetSashClassName/GetSashClassName.ts
@@ -1,5 +1,9 @@
 import * as LayoutDirection from '../LayoutDirection/LayoutDirection.ts'
 
+const classVertical = 'Sash SashVertical'
+
+const classHorizontal = 'Sash SashHorizontal'
+
 export const getSashClassName = (direction: 1 | 2): string => {
-  return direction === LayoutDirection.Horizontal ? 'Sash SashVertical' : 'Sash SashHorizontal'
+  return direction === LayoutDirection.Horizontal ? classVertical : classHorizontal
 }

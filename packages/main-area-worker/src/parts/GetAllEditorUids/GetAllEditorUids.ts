5eff1003eae1dc13f12744d9e4d4ee31014d38fe
Le Vivilet <levivilet@pm.me>
2026-08-04 08:48:56 -0700

    Simplify getAllEditorUids (#701)

--- packages/main-area-worker/src/parts/GetAllEditorUids/GetAllEditorUids.ts
+++ packages/main-area-worker/src/parts/GetAllEditorUids/GetAllEditorUids.ts
@@ -5,17 +5,6 @@ import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
  * Only returns editorUids that are not -1 (actually created viewlets).
  */
 export const getAllEditorUids = (state: MainAreaState): number[] => {
-  const { layout } = state
-  const { groups } = layout
-  const editorUids: number[] = []
-
-  for (const group of groups) {
-    for (const tab of group.tabs) {
-      if (tab.editorUid !== -1) {
-        editorUids.push(tab.editorUid)
-      }
-    }
-  }
-
-  return editorUids
+  const { groups } = state.layout
+  return groups.flatMap((group) => group.tabs.map((tab) => tab.editorUid)).filter((editorUid) => editorUid !== -1)
 }

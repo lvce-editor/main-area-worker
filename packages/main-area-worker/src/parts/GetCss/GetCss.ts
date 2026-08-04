1d6b0acdcdbb97d9aa0ddae1330ac1a402d5a8dc
Le Vivilet <levivilet@pm.me>
2026-08-04 08:51:10 -0700

    feature: simplify sash css (#696)

--- packages/main-area-worker/src/parts/GetCss/GetCss.ts
+++ packages/main-area-worker/src/parts/GetCss/GetCss.ts
@@ -4,16 +4,20 @@ import { getEditorGroupCss } from '../GetEditorGroupCss/GetEditorGroupCss.ts'
 import { getSashCorner } from '../GetSashCorner/GetSashCorner.ts'
 import { getSashCss } from '../GetSashCss/GetSashCss.ts'
 
+const getSashCornerCss = (left: number, top: number) => {
+  return `.SashCorner {
+  left: ${left}%;
+  top: ${top}%;
+}`
+}
+
 export const getCss = (layout?: MainAreaLayout, width: number = 0, dragOverlay?: DragOverlay): string => {
   const rules: string[] = []
   if (layout) {
     rules.push(...getEditorGroupCss(layout), ...getDragOverlayCss(dragOverlay), ...getSashCss(layout, width))
     const sashCorner = getSashCorner(layout)
     if (sashCorner) {
-      rules.push(`.SashCorner {
-  left: ${sashCorner.leftOffset}%;
-  top: ${sashCorner.topOffset}%;
-}`)
+      rules.push(getSashCornerCss(sashCorner.leftOffset, sashCorner.topOffset))
     }
   }
   const css = rules.join('\n')

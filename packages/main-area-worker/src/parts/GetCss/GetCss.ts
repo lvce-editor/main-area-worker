dab6b10a20a37f87e437d9b441872457a016c857
Le Vivilet <levivilet@pm.me>
2026-08-04 17:19:33 +0200

    feature: simplify sash css

--- packages/main-area-worker/src/parts/GetCss/GetCss.ts
+++ packages/main-area-worker/src/parts/GetCss/GetCss.ts
@@ -1,8 +1,15 @@
-import type { DragOverlay, MainAreaLayout } from '../MainAreaState/MainAreaState.ts'
 import { getDragOverlayCss } from '../GetDragOverlayCss/GetDragOverlayCss.ts'
 import { getEditorGroupCss } from '../GetEditorGroupCss/GetEditorGroupCss.ts'
 import { getSashCorner } from '../GetSashCorner/GetSashCorner.ts'
 import { getSashCss } from '../GetSashCss/GetSashCss.ts'
+import type { DragOverlay, MainAreaLayout } from '../MainAreaState/MainAreaState.ts'
+
+const getSashCornerCss = (left: number, top: number) => {
+  return `.SashCorner {
+  left: ${left}%;
+  top: ${top}%;
+}`
+}
 
 export const getCss = (layout?: MainAreaLayout, width: number = 0, dragOverlay?: DragOverlay): string => {
   const rules: string[] = []
@@ -10,10 +17,7 @@ export const getCss = (layout?: MainAreaLayout, width: number = 0, dragOverlay?:
     rules.push(...getEditorGroupCss(layout), ...getDragOverlayCss(dragOverlay), ...getSashCss(layout, width))
     const sashCorner = getSashCorner(layout)
     if (sashCorner) {
-      rules.push(`.SashCorner {
-  left: ${sashCorner.leftOffset}%;
-  top: ${sashCorner.topOffset}%;
-}`)
+      rules.push(getSashCornerCss(sashCorner.leftOffset, sashCorner.topOffset))
     }
   }
   const css = rules.join('\n')

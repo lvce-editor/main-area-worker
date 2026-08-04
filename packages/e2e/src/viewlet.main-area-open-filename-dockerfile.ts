fd9b40e108a440349938ce9726a073a08bd46462
Le Vivilet <levivilet@pm.me>
2026-08-04 13:38:38 +0000

    feature: add 50 main area filename e2e tests

---
+++ packages/e2e/src/viewlet.main-area-open-filename-dockerfile.ts
@@ -0,0 +1,17 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'viewlet.main-area-open-filename-dockerfile'
+
+export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  const fileName = 'Dockerfile'
+  const file = `${tmpDir}/${fileName}`
+  await FileSystem.writeFile(file, 'main area filename e2e test')
+
+  await Main.openUri(file)
+
+  const selectedTab = Locator(`.MainTabSelected[title$="${fileName}"]`)
+  await expect(selectedTab).toBeVisible()
+  const tabs = Locator('.MainTab')
+  await expect(tabs).toHaveCount(1)
+}

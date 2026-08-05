66a05c01c3b73955bfbf6e4c3a639bbc3bcb865b
Le Vivilet <levivilet@pm.me>
2026-08-05 10:16:56 +0000

    feature: use drag and drop worker

--- packages/main-area-worker/src/parts/ParseUriList/ParseUriList.ts
+++
@@ -1,8 +0,0 @@
-const lineSeparatorRegex = /\r?\n/
-
-export const parseUriList = (value: string): readonly string[] => {
-  return value
-    .split(lineSeparatorRegex)
-    .map((uri) => uri.trim())
-    .filter((uri) => uri && !uri.startsWith('#'))
-}
--- packages/main-area-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/main-area-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -77,7 +77,7 @@ export const renderEventListeners = (): readonly DomEventListener[] => {
     },
     {
       name: DomEventListenersFunctions.HandleDrop,
-      params: ['handleDrop', EventExpression.DataTransferFiles2, EventExpression.DataTransferFiles],
+      params: ['handleDrop', EventExpression.DataTransferFiles2],
       preventDefault: true,
     },
   ]

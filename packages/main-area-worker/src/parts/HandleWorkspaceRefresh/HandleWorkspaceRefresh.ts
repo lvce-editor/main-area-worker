13c45cae89e24749ecc6e603a6baeaa2c52ee308
Le Vivilet <levivilet@pm.me>
2026-08-09 08:38:06 +0000

    feature: reload editors after file changes

--- packages/main-area-worker/src/parts/HandleWorkspaceRefresh/HandleWorkspaceRefresh.ts
+++ packages/main-area-worker/src/parts/HandleWorkspaceRefresh/HandleWorkspaceRefresh.ts
@@ -1,12 +1,56 @@
 import type { AsyncCommandContext } from '@lvce-editor/viewlet-registry'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import type { EditorInput } from '../EditorInput/EditorInput.ts'
 import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
 import type { WorkspaceChanges, WorkspaceRefresh } from '../WorkspaceChanges/WorkspaceChanges.ts'
 import { closeTabWithViewlet } from '../CloseTabWithViewlet/CloseTabWithViewlet.ts'
 import { handleUriChange } from '../HandleUriChange/HandleUriChange.ts'
 
+const getEditorInputUris = (editorInput: EditorInput | undefined): readonly string[] => {
+  switch (editorInput?.type) {
+    case 'diff-editor':
+      return [editorInput.uriLeft, editorInput.uriRight]
+    case 'editor':
+    case 'image':
+    case 'video':
+    case 'webview':
+      return [editorInput.uri]
+    default:
+      return []
+  }
+}
+
+const getChangedEditorUid = (editorUid: number, editorInput: EditorInput | undefined, uri: string | undefined, changedUris: Set<string>) => {
+  if (editorUid === -1) {
+    return undefined
+  }
+  const uris = getEditorInputUris(editorInput)
+  if (uris.some((editorUri) => changedUris.has(editorUri)) || (uris.length === 0 && uri && changedUris.has(uri))) {
+    return editorUid
+  }
+  return undefined
+}
+
+const reloadChangedEditors = async (state: MainAreaState, changed: readonly string[]): Promise<void> => {
+  if (changed.length === 0) {
+    return
+  }
+  const changedUris = new Set(changed)
+  const editorUids = new Set<number>()
+  for (const group of state.layout.groups) {
+    for (const tab of group.tabs) {
+      const editorUid = getChangedEditorUid(tab.editorUid, tab.editorInput, tab.uri, changedUris)
+      if (editorUid !== undefined) {
+        editorUids.add(editorUid)
+      }
+    }
+  }
+  await Promise.allSettled(Array.from(editorUids, (editorUid) => RendererWorker.invoke('Viewlet.reload', editorUid)))
+}
+
 export const handleWorkspaceRefresh = async (state: MainAreaState, refresh: WorkspaceRefresh = {}): Promise<MainAreaState> => {
   const changes: WorkspaceChanges = Array.isArray(refresh) ? { deleted: refresh } : (refresh as WorkspaceChanges)
-  const { deleted = [], renamed = [] } = changes
+  const { changed = [], deleted = [], renamed = [] } = changes
   let newState = state
   for (const [oldUri, newUri] of renamed) {
     newState = await handleUriChange(newState, oldUri, newUri)
@@ -19,6 +63,7 @@ export const handleWorkspaceRefresh = async (state: MainAreaState, refresh: Work
       }
     }
   }
+  await reloadChangedEditors(newState, changed)
   return newState
 }
 

001b88f81093974c5994cce2356dc694acc565df
Le Vivilet <levivilet@pm.me>
2026-08-21 08:17:31 +0000

    feature: extract drag overlay equality helper

--- packages/main-area-worker/src/parts/HandleDragOver/HandleDragOver.ts
+++ packages/main-area-worker/src/parts/HandleDragOver/HandleDragOver.ts
@@ -1,20 +1,11 @@
 import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
 import { getDragOverlay } from '../GetDragOverlay/GetDragOverlay.ts'
-
-const isEqual = (oldOverlay: MainAreaState['dragOverlay'], newOverlay: MainAreaState['dragOverlay']): boolean => {
-  return (
-    oldOverlay?.x === newOverlay?.x &&
-    oldOverlay?.y === newOverlay?.y &&
-    oldOverlay?.width === newOverlay?.width &&
-    oldOverlay?.height === newOverlay?.height &&
-    oldOverlay?.splitDirection === newOverlay?.splitDirection
-  )
-}
+import { isDragOverlayEqual } from '../IsDragOverlayEqual/IsDragOverlayEqual.ts'
 
 export const handleDragOver = (state: MainAreaState, eventX: number, eventY: number): MainAreaState => {
   const { dragOverlay: oldDragOverlay } = state
   const dragOverlay = getDragOverlay(state, eventX, eventY)
-  if (isEqual(oldDragOverlay, dragOverlay)) {
+  if (isDragOverlayEqual(oldDragOverlay, dragOverlay)) {
     return state
   }
   return {

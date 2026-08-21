60644ddc53e5aa9b0c653516d7a69aa2a1ed929d
Le Vivilet <levivilet@pm.me>
2026-08-21 13:14:06 -0700

    fix: activate tabs from nested click targets (#739)

--- packages/main-area-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/main-area-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -14,7 +14,7 @@ export const renderEventListeners = (): readonly DomEventListener[] => {
     },
     {
       name: DomEventListenersFunctions.HandleClickTab,
-      params: ['handleClickTab', 'event.target.dataset.groupIndex', 'event.target.dataset.index', EventExpression.Button],
+      params: ['handleClickTab', 'event.currentTarget.dataset.groupIndex', 'event.currentTarget.dataset.index', EventExpression.Button],
     },
     {
       name: DomEventListenersFunctions.HandleDoubleClick,
--- packages/main-area-worker/test/RenderEventListeners.test.ts
+++ packages/main-area-worker/test/RenderEventListeners.test.ts
@@ -18,7 +18,12 @@ test('renderEventListeners should pass the mouse button to the tab click handler
   const result = RenderEventListeners.renderEventListeners()
   const listener = result.find((listener) => listener.name === DomEventListenerFunctions.HandleClickTab)
 
-  expect(listener?.params).toEqual(['handleClickTab', 'event.target.dataset.groupIndex', 'event.target.dataset.index', EventExpression.Button])
+  expect(listener?.params).toEqual([
+    'handleClickTab',
+    'event.currentTarget.dataset.groupIndex',
+    'event.currentTarget.dataset.index',
+    EventExpression.Button,
+  ])
 })
 
 test('renderEventListeners should pass tab indices to the double click handler', () => {

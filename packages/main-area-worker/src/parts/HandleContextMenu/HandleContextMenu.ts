7bbd14f0f7bb2cd8785fea2c3e1dcecef415773f
Le Vivilet <levivilet@pm.me>
2026-08-21 01:56:33 +0000

    fix: defer empty group context menu

--- packages/main-area-worker/src/parts/HandleContextMenu/HandleContextMenu.ts
+++ packages/main-area-worker/src/parts/HandleContextMenu/HandleContextMenu.ts
@@ -2,6 +2,7 @@ import { MenuEntryId } from '@lvce-editor/constants'
 import type { MainAreaState } from '../MainAreaState/MainAreaState.ts'
 import * as Assert from '../Assert/Assert.ts'
 import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
+import * as RendererProcess from '../RendererProcess/RendererProcess.ts'
 
 const NoGroupId = -1
 
@@ -14,6 +15,15 @@ export const handleContextMenu = async (state: MainAreaState, rawGroupId: string
   }
   const groupId = rawGroupId === '' ? NoGroupId : Number(rawGroupId)
   if (Number.isNaN(groupId)) {
+    return state
+  }
+  if (RendererProcess.isConnected()) {
+    setTimeout(() => {
+      void RendererProcess.invoke('Viewlet.forwardRendererWorkerCommand', 'ContextMenu.show2', uid, MenuEntryId.Main, x, y, {
+        groupId,
+        menuId: MenuEntryId.Main,
+      })
+    }, 0)
     return state
   }
   await ContextMenu.show2(uid, MenuEntryId.Main, x, y, {

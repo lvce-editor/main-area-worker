4d8a4aea7e93b226c609ab9777f5e88fe07a467e
Le Vivilet <levivilet@pm.me>
2026-08-22 07:59:21 +0000

    feature: reuse one page for e2e tests

--- packages/e2e/scripts/run-chromium-e2e.js
+++
@@ -1,7 +0,0 @@
-import { runBatchedE2E } from './run-batched-e2e.js'
-
-process.exitCode = await runBatchedE2E({
-  browser: 'chromium',
-  forwardedArgs: ['--reuse-page', ...process.argv.slice(2)],
-  testBatchSize: 20,
-})

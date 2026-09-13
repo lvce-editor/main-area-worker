fbd3faa914e1f824d191e0bd456d19e14021d6ae
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2026-09-13 22:28:37 +0000

    test: accept fractional vertical sash sizes

--- packages/e2e/src/viewlet.main-area-sash-vertical.ts
+++ packages/e2e/src/viewlet.main-area-sash-vertical.ts
@@ -3,7 +3,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 export const name = 'viewlet.main-area-sash-vertical'
 
 const firstGroupHeight = /^(?:[5-9]\d\d|1\d{3})(?:\.\d+)?px$/
-const secondGroupHeight = /^8\dpx$/
+const secondGroupHeight = /^8\d(?:\.\d+)?px$/
 const minimumGroupHeight = /^250(?:\.\d+)?px$/
 
 export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {

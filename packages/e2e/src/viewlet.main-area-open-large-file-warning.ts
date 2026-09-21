88db3431f9c09256e8b25c77835cd92740a2c821
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-21 22:20:57 +0000

    test: use native temporary directory

--- packages/e2e/src/viewlet.main-area-open-large-file-warning.ts
+++ packages/e2e/src/viewlet.main-area-open-large-file-warning.ts
@@ -3,7 +3,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 export const name = 'viewlet.main-area-open-large-file-warning'
 
 export const test: Test = async ({ Command, Editor, expect, FileSystem, Locator, Main, Settings }) => {
-  const testFile = 'file:///tmp/lvce-editor-large-file-warning.txt'
+  const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
+  const testFile = `${tmpDir}/lvce-editor-large-file-warning.txt`
   const testContent = 'large file content '.repeat(128)
   await Settings.update({ 'files.maxFileSizeMB': 0.001 })
   await FileSystem.writeFile(testFile, testContent)

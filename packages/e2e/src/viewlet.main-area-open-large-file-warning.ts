5c4a8d20af294cf83ac503792e58ea8efaf33e00
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-22 07:14:12 +0000

    test: stabilize large file acceptance

--- packages/e2e/src/viewlet.main-area-open-large-file-warning.ts
+++ packages/e2e/src/viewlet.main-area-open-large-file-warning.ts
@@ -19,7 +19,7 @@ export const test: Test = async ({ Command, Editor, expect, FileSystem, Locator,
   await Command.execute('Main.handleClickAction', 'open-large-file')
 
   await expect(warning).toBeHidden()
+  await Editor.shouldHaveText(testContent)
   const editorContent = Locator('.EditorContent')
   await expect(editorContent).toBeVisible()
-  await Editor.shouldHaveText(testContent)
 }
